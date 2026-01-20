c3ade4b6f4c95185177aefdb06f238d63aa9fec2
alvaroruizlorenzo7-bit <alvaroruizlorenzo7@gmail.com>
2026-01-20 08:57:54 +0000

    5.2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,12 +2,69 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use App\Form\PostFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class BlogController extends AbstractController
 {
+
+
+
+
+    #[Route('/blog/new', name: 'new_post')]
+    public function newPost(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();   
+            
+            // Quitamos los caracteres especiales del título para crear el slug
+            $post->setSlug($slugger->slug($post->getTitle()));
+            
+            // Guardamos el usuario que crea el post y los contadores a 0
+            $post->setPostUser($this->getUser());
+            $post->setNumLikes(0);
+            $post->setNumComments(0);
+
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($post);
+            $entityManager->flush();
+            return $this->render('blog/new_post.html.twig', array(
+                'form' => $form->createView()    
+            ));
+        }
+        return $this->render('blog/new_post.html.twig', array(
+            'form' => $form->createView()    
+        ));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  #[Route('/testimonial', name: 'testimonial')]
     public function testimonial(): Response
